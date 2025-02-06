d3fdf5deb348663b430a3ecae905205bf1ee5a0c
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2025-02-05 23:32:37 -0300

    Update AppDatathon.py

--- AppDatathon.py
+++ AppDatathon.py
@@ -67,9 +67,11 @@ elif pagina == "Metodologia":
 
 elif pagina == "Dashboard Interativo" :
     power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiNzFkOTBjMGMtYmM3ZC00YzE0LWI1YjYtNjdhNTE0MzE0NWIyIiwidCI6IjExZGJiZmUyLTg5YjgtNDU0OS1iZTEwLWNlYzM2NGU1OTU1MSIsImMiOjR9&pageName=cb3dc97fa06772a0514d"
-    iframe_code = f'<iframe width="90%" height="800" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>'
-
-    st.components.v1.html(iframe_code, height=600)
+    iframe_code = f"""
+    <iframe width="100%" height="600" src="{powerbi_url}" frameborder="0" allowFullScreen="true"></iframe>
+"""
+# Exibe o painel do Power BI
+st.components.v1.html(iframe_html, height=700)
   
 elif pagina == "MVP":
     st. image ('imagens/Passos-magicos-icon-cor.png')
