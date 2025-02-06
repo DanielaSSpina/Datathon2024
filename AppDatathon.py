e39472cbd5315bbcbd2a02964a4a1bac01865e94
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2025-02-05 22:25:38 -0300

    Update AppDatathon.py

--- AppDatathon.py
+++ AppDatathon.py
@@ -65,7 +65,7 @@ elif pagina == "Metodologia":
     st. image ('imagens/9.1 Visualização de Dados - Acurácia do modelo.png')
     st. image ('imagens/9.1 Gráfico - Acurácia do modelo.png')
 
-elif pagina == "Dashboard Interativo":
+elif pagina == "Dashboard Interativo" :
     power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiNTUxNjljY2YtOWJjMi00Y2IyLTgxOTItNGRlYzRjMmY3OWM1IiwidCI6IjExZGJiZmUyLTg5YjgtNDU0OS1iZTEwLWNlYzM2NGU1OTU1MSIsImMiOjR9"
     iframe_code = f'<iframe width="70%" height="1300" src="{power_bi_url}" frameborder="0" allowFullScreen="true"></iframe>'
 
